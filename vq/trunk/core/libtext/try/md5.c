70219fe02565a2181ba83ca219a9fc43591a7f6d
(no author) <(no author)@36831980-ddd6-0310-a275-bfd50d89a3dc>
2004-04-01 18:10:09 +0000

    CODE: fixes in md5.{c,h}1 - didn't compile CODE: fixes in try/md5.c CODE: hmac_md5 changed into C++ CODE: added namespace text to hmac_md5

--- vq/trunk/core/libtext/try/md5.c
+++ vq/trunk/core/libtext/try/md5.c
@@ -3,10 +3,10 @@
 
 int main()
 {
-    digest[16];
+    unsigned char digest[16];
     MD5_CTX ctx;
     MD5Init(&ctx);
-    MD5Update(&ctx,"asdasd",6);
+    MD5Update(&ctx,digest,6);
     MD5Final(digest,&ctx);
     return 0;
 }
