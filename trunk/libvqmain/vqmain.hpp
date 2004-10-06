e7563bf225edca98c47f8f748d0076e33f658e3c
new <new@36831980-ddd6-0310-a275-bfd50d89a3dc>
2004-10-06 22:32:50 +0000

    CODE: first version, takes VQ_HOME from environment and puts it in global variable VQ_HOME

---
+++ trunk/libvqmain/vqmain.hpp
@@ -0,0 +1,9 @@
+#ifndef __VQMAIN_HPP
+#define __VQMAIN_HPP
+
+/// from environment variable VQ_HOME, default /var/vq
+extern const char * VQ_HOME;
+
+int vqmain( int, char ** );
+
+#endif // ifndef __VQMAIN_HPP
