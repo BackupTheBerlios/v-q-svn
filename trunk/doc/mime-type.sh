35213b62555afbbbe0b974bea2b3ec14ad086c04
new <new@36831980-ddd6-0310-a275-bfd50d89a3dc>
2005-03-04 20:29:11 +0000

    DOC: corrected mime-type.sh

--- trunk/doc/mime-type.sh
+++ trunk/doc/mime-type.sh
@@ -2,7 +2,7 @@
 
 for p in $(find . -type f -iname "*.html") ; do
 	mt=$(svn pg svn:mime-type $p)
-	if [ $mt != "text/html" ] ; then
+	if [ X"$mt" != X"text/html" ] ; then
 		svn ps svn:mime-type text/html $p
 	fi
 done
